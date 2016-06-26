080b29d42cc2c65469e03ce8d6f10e7d8a5af85e
Michael Krukar <krukar@gmail.com>
2016-06-26 16:03:41 -0400

    Basic functionality complete

--- src/js/main.js
+++ src/js/main.js
@@ -1,50 +1,77 @@
 ///<reference path="d/jquery.d.ts" />
 'use strict';
 $(function () {
-    // Our main show object
-    var dates = [];
-    // let dates: { [s: string]: [{ [s: string]: string; }] } = {};
     // On submit
     $('#submit').on('click', function () {
+        var dates = []; // Our main show object
+        var startYear;
         var data = $('#textarea').val(); // text area data to parse
         var split = data.match(/[^\r\n]+/g); // split it up by lines
         for (var _i = 0, split_1 = split; _i < split_1.length; _i++) {
             var line = split_1[_i];
-            // just in case the line is emtpy, if it has less than 10 lines then there's no date so it's definitely broken
-            if (line.length > 10) {
-                var date = line.substring(0, 10); // get date
-                var info = line.substring(11, line.length - 1); // get everything after the date
-                info = info.replace('Report a problem', ''); // remove the trailing X
-                info = info.replace(/\s+/g, ' ').trim(); // remove extra spaces and trim the ends
+            var regx = line.match(/(20\d{2}-\d{2}-\d{2})(.+?(?=Report a problem))/);
+            // if the regx got no results
+            if (regx) {
+                var date = regx[1].trim();
+                var title = regx[2].trim();
                 var type = void 0;
-                if (info.match(/Season/g)) {
-                    type = 'TV Show';
+                if (title.match(/Season/g)) {
+                    type = 'tv';
                 }
                 else {
-                    type = 'Movie';
+                    type = 'movie';
                 }
                 if (dates[date] == null) {
                     dates[date] = [];
                 }
                 ;
                 var show = {
-                    title: info,
+                    date: date,
+                    title: title,
                     type: type
                 };
+                startYear = parseInt(date.substring(0, 4));
                 dates[date].push(show);
             }
         }
         ;
-        // Reverse the order since Netflix displays them latest first, this ensures you see the correct order
-        for (var date in dates) {
-            dates[date].reverse();
-        }
-        var lis = '';
-        for (var i = 0; i < 366; i++) {
-            lis += '<li class="day day_' + i + '"></li>';
-        }
-        $('#list2016').append(lis);
-        console.log(dates);
+        injectYears(startYear);
+        injectDates(dates);
     });
+    function injectYears(startYear) {
+        var leapYears = [2000, 2004, 2008, 2012, 2016, 2020];
+        var container = $('#lists');
+        container.html(''); // Clear the node before inserting in to it
+        var endYear = new Date().getFullYear();
+        var years = endYear - startYear + 1;
+        for (var i = 0; i < years; i++) {
+            var year = startYear + i;
+            container.append('<div id="year_' + year + '" class="year"><h1>' + year + '</h1><ul class="list"></ul></div>');
+            var days = 365;
+            if (leapYears.indexOf(year) > 0) {
+                days = 366;
+            }
+            var list = '';
+            for (var i_1 = 0; i_1 < days; i_1++) {
+                list += '<li class="day day_' + i_1 + '"></li>';
+            }
+            $('#year_' + year + ' .list').append(list);
+        }
+    }
+    function injectDates(dates) {
+        for (var date in dates) {
+            var regx = date.match(/(20\d{2})-(\d{2})-(\d{2})/);
+            var year = parseInt(regx[1]);
+            var month = parseInt(regx[2]) - 1;
+            var day = parseInt(regx[3]);
+            var start = new Date(year, 0, 1);
+            var end = new Date(year, month, day);
+            var doy = Math.floor((end - start) / (1000 * 60 * 60 * 24));
+            for (var content in dates[date]) {
+                var self_1 = dates[date][content];
+                $('.day', $('#year_' + year)).eq(doy).addClass(self_1.type);
+            }
+        }
+    }
 });
 console.log('main.ts');
--- src/ts/main.ts
+++ src/ts/main.ts
@@ -3,31 +3,27 @@
 
 $(function() {
 
-	// Our main show object
-	let dates: any = [];
-	// let dates: { [s: string]: [{ [s: string]: string; }] } = {};
-
 	// On submit
 	$('#submit').on('click', function(){
+		let dates: any = []; // Our main show object
+		let startYear: number;
 
 		let data: string = $('#textarea').val(); // text area data to parse
 		let split: string[] = data.match(/[^\r\n]+/g); // split it up by lines
 
 		for(let line of split){
-
-			// just in case the line is emtpy, if it has less than 10 lines then there's no date so it's definitely broken
-			if (line.length > 10) {
-				let date: string = line.substring(0, 10); // get date
-				let info: string = line.substring(11, line.length - 1); // get everything after the date
-				info = info.replace('Report a problem', ''); // remove the trailing X
-				info = info.replace(/\s+/g, ' ').trim(); // remove extra spaces and trim the ends
+			let regx: any = line.match(/(20\d{2}-\d{2}-\d{2})(.+?(?=Report a problem))/);
+			// if the regx got no results
+			if(regx){
+				let date: string = regx[1].trim();
+				let title: string = regx[2].trim();
 
 				let type: string;
-				if (info.match(/Season/g)) {
-					type = 'TV Show';
+				if (title.match(/Season/g)) {
+					type = 'tv';
 				}
 				else {
-					type = 'Movie';
+					type = 'movie';
 				}
 
 				if (dates[date] == null) {
@@ -35,32 +31,64 @@ $(function() {
 				};
 
 				let show: any = {
-					title: info,
+					date: date,
+					title: title,
 					type: type
 				};
 
-				dates[date].push(show);
-			}
+				startYear = parseInt(date.substring(0, 4));
 
+				dates[date].push(show);	
+			}			
 		};
 
-		// Reverse the order since Netflix displays them latest first, this ensures you see the correct order
-		for (let date in dates) {
-			dates[date].reverse();
-		}
-
-		let lis: string = '';
-
-		for (let i: number = 0; i < 366; i++){
-			lis += '<li class="day day_' + i + '"></li>';
-		}
-
-		$('#list2016').append(lis);
-
-		console.log(dates)
-
+		injectYears(startYear);
+		injectDates(dates);
 	});
 
+	function injectYears(startYear: number){
+		let leapYears: number[] = [2000, 2004, 2008, 2012, 2016, 2020];
+		let container: any = $('#lists');
+		container.html(''); // Clear the node before inserting in to it
+		let endYear: number = new Date().getFullYear();
+		let years: number = endYear - startYear + 1;
+		
+		for(let i: number = 0; i < years; i++){
+			let year: number = startYear + i;
+			container.append('<div id="year_' + year + '" class="year"><h1>' + year + '</h1><ul class="list"></ul></div>');
+
+			let days: number = 365;
+			if(leapYears.indexOf(year) > 0){
+				days = 366;
+			}
+
+			let list: string = '';
+			for(let i: number = 0; i < days; i++){
+				list += '<li class="day day_' + i + '"></li>';
+			}
+
+			$('#year_' + year + ' .list').append(list);
+		}
+	}
+
+	function injectDates(dates: any){
+		for(let date in dates){
+			let regx: any = date.match(/(20\d{2})-(\d{2})-(\d{2})/);
+			let year: number = parseInt(regx[1]);
+			let month: number = parseInt(regx[2]) - 1;
+			let day: number = parseInt(regx[3]);
+
+			let start: any = new Date(year, 0, 1);
+			let end: any = new Date(year, month, day);
+
+			let doy: number = Math.floor((end - start) / (1000 * 60 * 60 * 24));
+
+			for(let content in dates[date]){
+				let self: any = dates[date][content];
+				$('.day', $('#year_' + year)).eq(doy).addClass(self.type);
+			}
+		}
+	}
 }); 
 
 console.log('main.ts');   
